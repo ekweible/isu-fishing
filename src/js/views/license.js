f3fd19c09664e506528b29a6c6a5bf18fdf1d14f
ekweible <ekweible@gmail.com>
2014-08-07 18:39:31 -0500

    Add license page, link dnr and club links to websites, correct mcfarland park image

--- src/js/views/license.js
+++ src/js/views/license.js
@@ -1,5 +1,6 @@
 /** @jsx React.DOM */
 var React = require('react');
+var Grid = require('../components/Grid');
 
 module.exports = React.createClass({
     componentDidMount: function() {
@@ -8,6 +9,55 @@ module.exports = React.createClass({
         }, 500);
     },
     render: function() {
-        return <div>License</div>
+        return (
+            <Grid>
+                <h2>Fishing Licenses</h2>
+                <p className="lead">
+                    In order to fish in Iowa, you will need a fishing license. Fortunately, fishing licenses from
+                    the Iowa DNR are fairly inexpensive.
+                </p>
+
+                <p>To purchase a fishing license online, you can <a href="https://jc.activeoutdoorsolutions.com/ia_customer/app/goHome.do">
+                visit this site</a>.</p>
+                <br />
+
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>License</th>
+                            <th>Price</th>
+                            <th>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1 day</td>
+                            <td>$9.50</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>7 day</td>
+                            <td>$13.50</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>1 year</td>
+                            <td>$19.00</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>3 year</td>
+                            <td>$53.00</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Lifetime</td>
+                            <td>$52.50</td>
+                            <td>Must be 65 years or older</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Grid>
+        );
     }
 });
